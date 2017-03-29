c53a2e2e4cca434c795df795e5829e041f4732fa
Julian Nicholls <juliannicholls29@gmail.com>
2017-03-29 12:24:21 +0100

    Update Todo model constraints

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ApplicationRecord
 
-  validates :name, length: { minimum: 6 }
-  validates :description, length: { minimum: 6 }
+  validates :name, length: { minimum: 5 }
+  validates :description, length: { minimum: 12 }
 
 end
